489fa8e1684b085157cbf9a540d6574674a93165
Nechai_Ihor <fredduke1983@gmail.com>
2023-02-05 13:05:42 +0200

    up

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
-
 const DEBOUNCE_DELAY = 300;
+
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
+  );
+}
+
+const newResCountry = fetchCountries('Uk');
+let findCountry = null;
+
+newResCountry
+  .then(response => {
+    return response.json();
+  })
+  .then(r => {
+    findCountry = r;
+    return console.log(findCountry);
+  });
