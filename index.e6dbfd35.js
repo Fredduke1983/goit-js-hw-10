158ef65c6046110f376ad80402e064823f6a00cb
Fredduke1983 <fredduke1983@gmail.com>
2023-02-05 11:38:10 +0000

    Deploying to gh-pages from @ Fredduke1983/goit-js-hw-10@19e9174c5e3e636f28ab500328877d663c12525b 🚀

--- index.e6dbfd35.js
+++
@@ -1,2 +0,0 @@
-const e=fetch(`https://restcountries.com/v2/name/${"Uk"}?fields=name,capital,population,flags,languages`);let n=null;e.then((e=>e.json())).then((e=>(n=e,console.log(n))));
-//# sourceMappingURL=index.e6dbfd35.js.map
