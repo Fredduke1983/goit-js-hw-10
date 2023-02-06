05a1ba2c49aed1471ffe13665bdb4aa13682c123
Nechai_Ihor <fredduke1983@gmail.com>
2023-02-06 14:07:02 +0200

    up

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import { debounce, remove } from 'lodash';
 const DEBOUNCE_DELAY = 300;
 
 let countryFromInput = null;
-let findCountry = null;
 
 const countryInfo = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
+
 const searchBox = document.getElementById('search-box');
-searchBox.addEventListener('input', debounce(respon, 2000));
+searchBox.addEventListener('input', debounce(respon, 1000));
 
 function fetchCountries(name) {
   return fetch(
@@ -18,20 +19,23 @@ function fetchCountries(name) {
 function respon(e) {
   countryFromInput = e.target.value;
   const newResCountry = fetchCountries(`${countryFromInput}`);
+  let countries = [];
 
   newResCountry
     .then(response => {
       return response.json();
     })
-    .then(r => {
-      findCountry = r;
-      return findCountry.forEach(element => {
-        console.log(element.name);
-
-        countryInfo.insertAdjacentHTML(
-          'beforeend',
-          `<img src=${element.flags.svg} width="50px" height="25px"></img>`
-        );
-      });
+    .then(country => {
+      console.log(country.length);
+      if (country.length > 1 && country.length < 11) {
+        country.map(element => {
+          countries += `<img src=${element.flags.svg} width="50px" height="25px"></img>`;
+        });
+      } else if (country.length > 10) {
+        console.error('too many');
+      } else {
+        countryList.innerHTML = `<li><img src=${country[0].flags.svg} width="50px" height="25px"></img></li>`;
+      }
+      countryInfo.innerHTML = countries;
     });
 }
