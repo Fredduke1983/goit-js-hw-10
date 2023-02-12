d028163a93fca35d567f284521ac2d97a09a8344
Nechai_Ihor <fredduke1983@gmail.com>
2023-02-12 16:27:23 +0200

    up

--- src/js/onerror.js
+++ src/js/onerror.js
@@ -1,9 +1,9 @@
 import Notiflix from 'notiflix';
-import { countryInfo, countryList } from './vars.js';
+import vars from './vars.js';
 
 export default function onError(error) {
-  console.log(countryInfo);
-  countryInfo.innerHTML = '';
-  countryList.innerHTML = '';
+  console.log(error);
+  vars.countryInfo.innerHTML = '';
+  vars.countryList.innerHTML = '';
   Notiflix.Notify.failure('Oops, there is no country with that name');
 }
